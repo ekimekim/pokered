24d2c2466d7202a092bd272fd72fef449ab2a5a8
Mike Lang <mikelang3000@gmail.com>
2019-08-20 03:47:55 -0700

    fix problem with playing ch3: shift reg was set to mute

--- home/audio.asm
+++ home/audio.asm
@@ -263,6 +263,8 @@ WaveMusicStart::
 	ld a, [rNR51]
 	or %01000100 ; enable ch3 in mux
 	ld [rNR51], a
+	ld a, %00100000 ; no shift of samples
+	ld [rNR32], a
 	ld hl, rNR34
 	set 7, [hl] ; play
 	ret
